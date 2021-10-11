9f23f2aa30f5bce8cf7efcc8179f3b38c1f54199
Rick Hung <grayrain1030@gmail.com>
2021-10-12 01:35:45 +0800

    加入專案檔案。

---
+++ Models/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;    //有需要與資料庫連線，使用連線字串，就必須要引用 Microsoft.EntityFrameworkCore
+
+namespace TodoApi.Models
+{
+	public class TodoContext : DbContext
+	{
+		
+		public TodoContext(DbContextOptions<TodoContext> options)
+			: base(options) { }
+
+		public DbSet<TodoItem> TodoItems { get; set; }
+	}
+}
---
+++ Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;    //使用資料庫就必需要有這個套件
+using TodoApi.Models;					//指向Models資料夾
+
+namespace TodoApi
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+
+			//services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));		//使用記憶體資料庫
+			services.AddDbContext<TodoContext>(opt => opt.UseSqlite("Data Source=TodoApiDb.db"));	//使用Sqlite
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
